59ea57a873fcd4bbde262325c445cd6d7f8e0dc7
Reury Cardoso <reurycardoso7@gmail.com>
2025-01-08 14:26:42 -0300

    refactor: standardize html class names

--- src/page/login/login.tsx
+++ src/page/login/login.tsx
@@ -6,10 +6,10 @@ export function Login() {
       <h1 className="welcome-heading">Bem-vindo(a) à Taqtile!</h1>
       <form className='form-login' action=''>
         <label htmlFor='input-email'>E-mail</label>
-        <input className='inputs-form-login' id='input-email' type='email' required/>
+        <input className='form-login-inputs' id='input-email' type='email' required/>
 
         <label htmlFor='input-password'>Senha</label>
-        <input className='inputs-form-login' id='input-password' type='password' required/>
+        <input className='form-login-inputs' id='input-password' type='password' required/>
 
         <button className='form-login-button' type='submit'>Entrar</button>
       </form>
