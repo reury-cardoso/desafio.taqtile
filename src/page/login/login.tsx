b106ce71868d0fa22f520159e1e76032551e68d5
Reury Cardoso <reurycardoso7@gmail.com>
2025-01-08 17:31:52 -0300

    style: modifications to the style and structure of tags

--- src/page/login/login.tsx
+++ src/page/login/login.tsx
@@ -3,15 +3,21 @@ import './login.css';
 export function Login() {
   return (
     <div>
-      <h1 className="welcome-heading">Bem-vindo(a) à Taqtile!</h1>
+      <h1 className='welcome-heading'>Bem-vindo(a) à Taqtile!</h1>
       <form className='form-login' action=''>
-        <label htmlFor='input-email'>E-mail</label>
-        <input className='form-login-inputs' id='input-email' type='email' required/>
+        <div className='form-group'>
+          <label htmlFor='input-email'>E-mail</label>
+          <input className='form-login-inputs' id='input-email' type='email' />
+        </div>
 
-        <label htmlFor='input-password'>Senha</label>
-        <input className='form-login-inputs' id='input-password' type='password' required/>
+        <div className='form-group'>
+          <label htmlFor='input-password'>Senha</label>
+          <input className='form-login-inputs' id='input-password' type='password' />
+        </div>
 
-        <button className='form-login-button' type='submit'>Entrar</button>
+        <button className='form-login-button' type='submit'>
+          Entrar
+        </button>
       </form>
     </div>
   );
