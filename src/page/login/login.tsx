ec5ffeacc1b2cd429f16c232442e271f783f1009
Reury Cardoso <reurycardoso7@gmail.com>
2025-01-09 16:21:19 -0300

    feat: add success message

--- src/page/login/login.tsx
+++ src/page/login/login.tsx
@@ -13,25 +13,25 @@ type LoginFormData = {
 };
 
 export function Login() {
-  const [login, { loading, error: authError, data: authData }] = useMutation(LOGIN_MUTATION);
+  const [login, { loading, error, data }] = useMutation(LOGIN_MUTATION);
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors: formErrors },
   } = useForm<LoginFormData>({ resolver: zodResolver(loginSchema) });
 
-  const onSubmitLogin = async (data: LoginFormData) => {
+  const onSubmitLogin = async (formData: LoginFormData) => {
     try {
       const response = await login({
         variables: {
-          data,
+          data: formData,
         },
       });
       const token = response.data.login.token;
       localStorage.setItem('token', token);
-    } catch (error) {
-      console.log('Error on login:', authData);
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -42,19 +42,20 @@ export function Login() {
         <div className='form-group'>
           <label htmlFor='input-email'>E-mail</label>
           <input className='form-login-inputs' id='input-email' type='text' {...register('email')} />
-          {errors.email && <p className='error'>{errors.email.message}</p>}
+          {formErrors.email && <p className='error'>{formErrors.email.message}</p>}
         </div>
 
         <div className='form-group'>
           <label htmlFor='input-password'>Senha</label>
           <input className='form-login-inputs' id='input-password' type='password' {...register('password')} />
-          {errors.password && <p className='error'>{errors.password.message}</p>}
+          {formErrors.password && <p className='error'>{formErrors.password.message}</p>}
         </div>
 
         <button className='form-login-button' type='submit' disabled={loading}>
           {loading ? <img src={loadingCircles} alt='Loading...' /> : 'Entrar'}
         </button>
-        {authError && <p className='error'>{authError.message}</p>}
+        {error && <p className='error'>{error.message}</p>}
+        {data && <p className='success'>Login efetuado com sucesso!</p>}
       </form>
     </div>
   );
