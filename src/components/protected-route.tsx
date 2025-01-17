8eab8bb07d87319b45f21be5be0324cb6a7ac046
Reury Cardoso <reurycardoso7@gmail.com>
2025-01-17 20:10:53 -0300

    fix: standardize eslint

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -3,18 +3,18 @@ import { Navigate, Outlet } from 'react-router-dom';
 export function ProtectedRoute() {
   const token = localStorage.getItem('token');
 
-  if (!token) return <Navigate to="/auth" replace />;
+  if (!token) return <Navigate to='/auth' replace />;
 
   try {
     const jwt = token.split(' ')[1];
     const { exp } = JSON.parse(atob(jwt.split('.')[1]));
     if (Date.now() >= exp * 1000) {
       localStorage.removeItem('token');
-      return <Navigate to="/auth" replace />;
+      return <Navigate to='/auth' replace />;
     }
   } catch (error) {
     localStorage.removeItem('token');
-    return <Navigate to="/auth" replace />;
+    return <Navigate to='/auth' replace />;
   }
 
   return <Outlet />;
